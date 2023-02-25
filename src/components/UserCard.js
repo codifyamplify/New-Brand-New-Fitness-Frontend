6a9f5b7518e2d8e1fc57eacd5ff68a0408b1637f
David Whitney <codifyamplify@gmail.com>
2023-02-24 22:37:40 -0500

    create iteration for exercises on user card

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -3,12 +3,13 @@ import React from "react";
 function UserCard({user}){
     // const {name} = user.name
     // console.log("from card")
-    // console.log(users)
+    const exercises = user.exercises.map((exercise) => <li key={exercise.id}>{JSON.stringify(exercise)}</li>)
     
     return (
         <div>
             <h1>UserCard Component</h1>
             <h3>user name: {user.name}</h3>
+            <ul>{exercises}</ul>
         </div>
     )
 }
