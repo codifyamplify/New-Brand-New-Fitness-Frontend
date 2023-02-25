cdbfdd08e08e7836cb36a3a5edc71cb25d60b2f8
David Whitney <codifyamplify@gmail.com>
2023-02-24 21:19:00 -0500

    ran create react app, cleanup, start notes file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* className="App-header" for dark blue background */}
+      
+    </div>
+  );
+}
+
+export default App;
